a30e563dda44f771944e62bf9e312d45d2634356
Aryan Chaturvedi <aryanc23@iitk.ac.in>
2026-03-29 18:18:30 +0530

    updaring vercel.json

--- src/data/team.js
+++ src/data/team.js
@@ -46,22 +46,22 @@ export const team = [
         id: "c3",
         name: "Pathak Prince",
         role: "Events and Outreach",
-        image: "",
-        linkedin: "www.linkedin.com/in/aryan-chaturvedi-a84560206",
+        image: "https://media.licdn.com/dms/image/v2/D4E03AQHjXgZLZ57pMg/profile-displayphoto-scale_200_200/B4EZjhrUtsHgAc-/0/1756132889443?e=1776297600&v=beta&t=vFFNoPglF3F4ToyuB1Fn7cwcdMYG3qVEZvSRu-3ksdA",
+        linkedin: "https://www.linkedin.com/in/prince-pathak-1487502b6/",
       },
       {
         id: "c4",
         name: "Aarush Singh",
         role: "Events and Outreach",
-        image: "",
-        linkedin: "www.linkedin.com/in/aryan-chaturvedi-a84560206",
+        image: "https://media.licdn.com/dms/image/v2/D5603AQGTeV-zPvhUuQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1721586412100?e=1776297600&v=beta&t=0nQ6nv6S3aIgqNKR0AsB-n6P48zFGLQbA2Cx3dmm3hs",
+        linkedin: "https://www.linkedin.com/in/aarush-singh-95447427b/",
       },
       {
         id: "c5",
         name: "Harsh Maheshwari",
         role: "Events and Outreach",
-        image: "",
-        linkedin: "www.linkedin.com/in/aryan-chaturvedi-a84560206",
+        image: "https://media.licdn.com/dms/image/v2/D4E03AQEXfQaVc1qwAw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1708636958167?e=1776297600&v=beta&t=WUOFyWlMI0jIIQLTnUUZvZicDd1lMHBoNXc4jLtVms4",
+        linkedin: "https://www.linkedin.com/in/harshmaheshwari18/",
       }
     ]
   },
